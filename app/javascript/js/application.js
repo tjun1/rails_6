14e58797c48b4b84822e42045f58d989dee8bced
tjun1 <gaiant.baba@gmail.com>
2020-12-28 22:49:34 +0900

    1st commit.

---
+++ app/javascript/js/application.js
@@ -0,0 +1,4 @@
+import "./_uikit";
+import "../css/application.scss";
+// import hello_world from './hello_world';
+// hello_world();
---
+++ app/javascript/js/hello_world.js
@@ -0,0 +1,5 @@
+export default () => {
+  window.onload = () => {
+    document.getElementsByTagname("H1")[0].innerText = 'Hello World';
+  }
+}
